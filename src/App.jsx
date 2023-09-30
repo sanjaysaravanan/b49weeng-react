35d753ed89a25e4db6b7e6339653031489571b92
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-09-30 12:42:04 +0530

    added few more changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,57 @@ import './App.css'
 import Movie from './components/Movie';
 
 const movies = [
-  { name: 'Jawan', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/3/39/Jawan_film_poster.jpg', lead: 'Shah Rukh Khan' },
-  { name: 'Jailer', imageUrl: 'https://static.toiimg.com/photo/102602413.cms?imgsize=963075', lead: 'Rajinikanth' }
+  {
+    id: 1,
+    name: 'Jawan',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/en/3/39/Jawan_film_poster.jpg',
+    lead: 'Shah Rukh Khan'
+  },
+  {
+    id: 2,
+    name: 'Jailer',
+    imageUrl: 'https://static.toiimg.com/photo/102602413.cms?imgsize=963075',
+    lead: 'Rajinikanth'
+  },
+  {
+    id: 3,
+    name: 'Fukr3y',
+    imageUrl: 'https://assetscdn1.paytm.com/images/cinema/fukrey__705x750-a9be4510-5b7e-11ee-8273-793d5ca08c06.jpg',
+    lead: 'Some Bollywood Actor'
+  }
 ]
 
 function App() {
 
   return (
     <>
-      <Movie title={movies[0].name} lead={movies[0].lead} />
-      <Movie title={movies[1].name} lead={movies[1].lead} />
+      <div></div>
+      <div></div>
+      {/* {[
+        <div></div>,
+        <div></div>
+      ]} */}
+      {/* <Movie title={movies[0].name} lead={movies[0].lead} />
+      <Movie
+        title={movies[1].name}
+        lead={movies[1].lead}
+      />
+      <Movie title={movies[2].name} lead={movies[2].lead} /> */}
+      {/* {[
+        <Movie title={movies[0].name} lead={movies[0].lead} key={movies[0].name} />,
+        <Movie
+          title={movies[1].name}
+          lead={movies[1].lead}
+          key={movies[1].name}
+        />,
+        <Movie title={movies[2].name} lead={movies[2].lead} key={movies[2].name} />
+      ]} */}
+      {movies.map((movie) => <Movie
+        title={movie.name}
+        imageUrl={movie.imageUrl}
+        lead={movie.lead}
+        key={movie.id}
+      />)}
     </>
   )
 }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,29 @@
+import PropTypes from 'prop-types';
 
+// import './movie.css';
+
+// import styles from './movie.module.css';
+
+import { movieCard, divContainer } from './movie.module.css';
 
 function Movie(props) {
   return (
-    <div className="movie-card bg-primary">
+    // className="movie-card div-container"
+    // <div className={`${styles['movieCard']} ${styles['divContainer']}`} >
+    <div className={`${movieCard} ${divContainer}`} >
+      {/* <img src='https://image.com/image.png' style="height: 200px; width: 100px" />*/}
+      <img src={props.imageUrl} style={{ height: '200px', width: '150px' }} />
       Title: {props.title}
       <br />
       Lead: {props.lead}
     </div>
   )
 }
+
+Movie.propTypes = {
+  title: PropTypes.string,
+  lead: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
+};
 
 export default Movie;
